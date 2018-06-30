516fd4ee8e28735d503ebcab235482cbbb178827
Glen Simpson <gsimpson@airmaster.com.au>
2018-06-30 23:33:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 const app = "I don't do much."
+kittens = ['Milo', 'Otis', 'Garfield']
+
+
+function destructivelyAppendKitten(s) {
+  return kittens.push(s);
+}
+
+function destructivelyPrependKitten(s) {
+  return kittens.unshift(s);
+}
+
+function destructivelyRemoveLastKitten() {
+  return kittens.pop()
+}
+
+function destructivelyRemoveFirstKitten() {
+  return kittens.shift()
+}
+
+function appendKitten(s) {
+  return kittens.concat(s)
+}
+
+function prependKittens(s) {
+  return
+}
